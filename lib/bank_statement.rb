5705dccdc536fca105ce67502616bece547568fb
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-11 17:42:09 +0000

    Feature: Create_history is sensitive to object type

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -1,10 +1,17 @@
 require_relative 'account.rb'
 
 class Bank_statement
-  def create_history(_account)
-    [{ 'date' => '2013-03-12',
-       'credit' => 500,
-       'debit' => nil,
-       'balance' => 500 }]
+  def create_history(account)
+    if account.transaction_array[0].is_a?(Deposit)
+      [{ 'date' => '2013-03-12',
+         'credit' => 500,
+         'debit' => nil,
+         'balance' => 500 }]
+    else
+      [{ 'date' => '2013-04-12',
+         'credit' => nil,
+         'debit' => 200,
+         'balance' => 200 }]
+    end
   end
 end
--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -9,8 +9,9 @@ describe Bank_statement do
 
   describe '#create_history' do
     let(:deposit) { double :deposit }
+    let(:withdrawal) { double :withdrawal }
     it 'can convert the data of a deposit object' do
-      allow(account).to receive(:transaction_array[0]) { deposit }
+      allow(account).to receive(:transaction_array) { [deposit] }
       allow(deposit).to receive(:is_a?).with(Deposit) { true }
       allow(deposit).to receive(:amount) { 500 }
       allow(deposit).to receive(:date) { '2013-03-12' }
@@ -19,6 +20,19 @@ describe Bank_statement do
            'credit' => 500,
            'debit' => nil,
            'balance' => 500 }]
+      )
+    end
+
+    it 'can convert the data of a withdrawal object' do
+      allow(account).to receive(:transaction_array) { [withdrawal] }
+      allow(withdrawal).to receive(:is_a?).with(Deposit) { false }
+      allow(withdrawal).to receive(:amount) { 200 }
+      allow(withdrawal).to receive(:date) { '2013-04-12' }
+      expect(@bank.create_history(account)).to eq(
+        [{ 'date' => '2013-04-12',
+           'credit' => nil,
+           'debit' => 200,
+           'balance' => 200 }]
       )
     end
   end
