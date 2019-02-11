3dcb76f1f08f72a740a439eb3917f8dab055da59
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-11 15:09:11 +0000

    Feature: withdrawal can be added to account and saved to array

--- lib/account.rb
+++ lib/account.rb
@@ -2,4 +2,13 @@ require_relative 'deposit.rb'
 require_relative 'withdrawal.rb'
 
 class Account
+  attr_reader :transaction_array
+
+  def initialize
+    @transaction_array = []
+  end
+
+  def create_withdrawal(amount, withdrawal = Withdrawal.new(amount))
+    @transaction_array.push(withdrawal)
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,13 @@
 require 'account'
 
 describe Account do
-  describe ''
+  describe '#create_withdrawal' do
+    let (:withdrawal) { double :withdrawal }
+    account = Account.new
+    it 'adds to transaction_array' do
+      expect { account.create_withdrawal(nil, withdrawal) }.to(
+        change { account.transaction_array.length }.by(1)
+      )
+    end
+  end
 end
