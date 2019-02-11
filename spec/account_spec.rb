db0476499b2cf273aae9af51dcc61d6b6f1975cf
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-11 15:23:11 +0000

    Feature: create deposit saves a deposit to an account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,12 +1,24 @@
 require 'account'
 
 describe Account do
+  before do
+    @account = Account.new
+  end
+
   describe '#create_withdrawal' do
     let (:withdrawal) { double :withdrawal }
-    account = Account.new
     it 'adds to transaction_array' do
-      expect { account.create_withdrawal(nil, withdrawal) }.to(
-        change { account.transaction_array.length }.by(1)
+      expect { @account.create_withdrawal(nil, withdrawal) }.to(
+        change { @account.transaction_array.length }.by(1)
+      )
+    end
+  end
+
+  describe '#create_deposit' do
+    let (:deposit) { double :deposit }
+    it 'adds to transaction_array' do
+      expect { @account.create_withdrawal(nil, deposit) }.to(
+        change { @account.transaction_array.length }.by(1)
       )
     end
   end
