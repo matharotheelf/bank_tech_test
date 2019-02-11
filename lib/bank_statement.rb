dd61a8bafb48f33fd72bc2162c05c6e615ff6ecf
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-11 17:20:09 +0000

    Feature: added create_history to bank statement

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -1,2 +1,10 @@
+require_relative 'account.rb'
+
 class Bank_statement
+  def create_history(_account)
+    [{ 'date' => '2013-03-12',
+       'credit' => 500,
+       'debit' => nil,
+       'balance' => 500 }]
+  end
 end
--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -1,4 +1,25 @@
 require 'bank_statement'
 
 describe Bank_statement do
+  before do
+    @bank = Bank_statement.new
+  end
+
+  let(:account) { double :account }
+
+  describe '#create_history' do
+    let(:deposit) { double :deposit }
+    it 'can convert the data of a deposit object' do
+      allow(account).to receive(:transaction_array[0]) { deposit }
+      allow(deposit).to receive(:is_a?).with(Deposit) { true }
+      allow(deposit).to receive(:amount) { 500 }
+      allow(deposit).to receive(:date) { '2013-03-12' }
+      expect(@bank.create_history(account)).to eq(
+        [{ 'date' => '2013-03-12',
+           'credit' => 500,
+           'debit' => nil,
+           'balance' => 500 }]
+      )
+    end
+  end
 end
