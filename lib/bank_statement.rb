192179e8db5fe998705a83efa71a9dcf2d4018e2
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-12 12:33:21 +0000

    Refactor: Creating hash refactored to new create_hash method from create_history

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -7,21 +7,26 @@ class Bank_statement
     balance = 0
     sorted = transactions.sort_by { |s| Date.strptime(s.date, '%Y-%m-%d') }
     sorted.each do |element|
-      hash = {}
-      hash['date'] = element.date
-      if element.is_a?(Deposit)
-        hash['credit'] = element.amount
-        balance += element.amount
-        hash['balance'] = balance
-        hash['debit'] = nil
-      else
-        hash['debit'] = element.amount
-        balance -= element.amount
-        hash['balance'] = balance
-        hash['credit'] = nil
-      end
-      final_array.unshift(hash)
+      final_array.unshift(create_hash(element, balance))
+      balance = create_hash(element, balance)['balance']
     end
     final_array
   end
+end
+
+def create_hash(element, balance)
+  hash = {}
+  hash['date'] = element.date
+  if element.is_a?(Deposit)
+    hash['credit'] = element.amount
+    balance += element.amount
+    hash['balance'] = balance
+    hash['debit'] = nil
+  else
+    hash['debit'] = element.amount
+    balance -= element.amount
+    hash['balance'] = balance
+    hash['credit'] = nil
+  end
+  hash
 end
