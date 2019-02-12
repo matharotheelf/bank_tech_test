fba6bf927b03329f6af967434579fae100a84f5a
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-12 14:56:53 +0000

    Feature: Added print_statement method to Print_statement class it returns statement string

--- lib/print_statement.rb
+++ lib/print_statement.rb
@@ -1,4 +1,29 @@
 require_relative 'bank_statement.rb'
 
 class Print_statement
+  def print_statement(statement_array)
+    str = "date || credit || debit || balance\n"
+    statement_array.each do |i|
+      str += "#{i['date']} || #{credit(i)} || #{debit(i)} || #{i['balance']}\n"
+    end
+    str
+  end
+
+  private
+
+  def credit(hash)
+    if hash['credit'].nil?
+      ''
+    else
+      hash['credit']
+    end
+  end
+
+  def debit(hash)
+    if hash['debit'].nil?
+      ''
+    else
+      hash['debit']
+    end
+  end
 end
--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -1,4 +1,21 @@
 require 'print_statement'
 
 describe Print_statement do
+  describe '#print_statement' do
+    statement_array = [{ 'date' => '2013-09-12',
+                         'credit' => nil,
+                         'debit' => 2,
+                         'balance' => 3 },
+                       { 'date' => '2013-05-12',
+                         'credit' => 5,
+                         'debit' => nil,
+                         'balance' => 5 }]
+    it 'can create statement string' do
+      expect(subject.print_statement(statement_array)).to eq(
+        "date || credit || debit || balance\n" \
+        "2013-09-12 ||  || 2 || 3\n" \
+        "2013-05-12 || 5 ||  || 5\n"
+      )
+    end
+  end
 end
